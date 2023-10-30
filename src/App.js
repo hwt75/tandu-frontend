2bd9a8f26ee327ea1fd46c8fb16fd6c68e6eb77e
hwt75 <dnthang300@gmail.com>
2023-10-30 22:13:28 +0700

    hwt update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
 import { Layout } from "./layouts/layout";
-
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 export const App = () => {
   return (
-    <HashRouter>
-   <Layout/>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <Layout />
+      </HashRouter>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/redux/dataSlice.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const dataSlice = createSlice({
+    name: 'data',
+    initialState: {},
+    reducers: {},
+    extraReducers: {}
+})
+
+export default dataSlice.reducer;
