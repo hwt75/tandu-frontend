ba910fd3d61879dfdbe982d91647b2eaf2db82fd
Huyz109 <phamqhuy2001@gmail.com>
2023-11-02 23:42:28 +0700

    update home page

--- src/components/countrySection/countrySection.jsx
+++ src/components/countrySection/countrySection.jsx
@@ -9,7 +9,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 export const CountrySection = () => {
-   
     return (
         <>
             <div className='section container text-center'>
